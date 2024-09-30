2d05d603538f5a83c7d593b2102989a145c847a1
Leandro Maximiliano <32625973+Maxpach@users.noreply.github.com>
2024-09-30 09:18:11 -0300

    Initial commit

---
+++ forms.py
@@ -0,0 +1,28 @@
+#criar os formulario
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed
+from wtforms import StringField, SubmitField, PasswordField, FileField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from fakepinterest.models import Usuario
+
+
+class FormLogin(FlaskForm):
+    email = StringField("E-mail", validators=[DataRequired()])
+    senha = PasswordField("Senha", validators=[DataRequired()])
+    botao_confirmacao = SubmitField("Fazer login")
+
+class FormCriarConta(FlaskForm):
+    email = StringField("E-mail", validators=[DataRequired()])
+    username = StringField("Nome de usuário ", validators=[DataRequired()])
+    senha = PasswordField("Senha", validators=[DataRequired(), Length(6, 20)])
+    confirmacao_senha = PasswordField("Confirmação de senha", validators=[DataRequired(), EqualTo("senha")])
+    botao_confirmacao = SubmitField("Criar conta")
+
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        if usuario:
+            return ValidationError("E-mail já cadastrado")
+
+class FormFoto(FlaskForm):
+    foto = FileField("Foto", validators=[DataRequired()])
+    botao_confirmacao = SubmitField("Enviar")
